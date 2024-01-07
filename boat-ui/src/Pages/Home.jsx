241cb07b61c6c629391d74d8ffba81b664803baf
JahirPendhari <jahirpp1999@gmail.com>
2024-01-07 12:41:43 +0530

    Home page All Banner Added

--- boat-ui/src/Pages/Home.jsx
+++ boat-ui/src/Pages/Home.jsx
@@ -1,10 +1,34 @@
-const Homepage =()=>{
+const Homepage = () => {
     return <div>
-        <div style={{width:"100%",backgroundColor:"lightBlue"}}>
+        <div style={{ width: "100%", backgroundImage: "linear-gradient(#edfefe, #eeffff)" }}>
 
-           <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_1_desktop_4c81b094-8292-4d54-8b20-5eb3b823a4e6_2000x.png?v=1686650857" alt="banner" width="50%" style={{ marginLeft:"45%"}}/>
+            <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_1_desktop_4c81b094-8292-4d54-8b20-5eb3b823a4e6_2000x.png?v=1686650857" alt="banner" width="50%" style={{ marginLeft: "45%" }} />
+        </div>
+        <div style={{ width: "100%", backgroundImage: "linear-gradient(#ffffff, #fff1d6)" }}>
+
+            <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_2_mob_390x.png?v=1686117497" alt="banner" width="50%" style={{ marginRight: "45%" }} />
+        </div>
+        <div style={{ width: "100%", backgroundImage: "linear-gradient(#ffffff, #f2edff)" }}>
+
+            <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_3_mob_1282x.png?v=1686131082" alt="banner" width="50%" style={{ marginLeft: "45%" }} />
+        </div>
+        <div style={{ width: "100%", backgroundImage: "linear-gradient(#ffffff, #dfeeeb)" }}>
+
+            <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_4_desktop_e51eb905-3de3-4323-8dc3-b2362bd879e2_2000x.png?v=1686650793" alt="banner" width="50%" style={{ marginRight: "45%" }} />
+        </div>
+        <div style={{ width: "100%", backgroundImage: "linear-gradient(#ffffff, #e6ecff)" }}>
+
+            <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_5_desktop_993080a5-771b-4fbd-961a-a4dae9e55838_2000x.png?v=1686650885" alt="banner" width="50%" style={{ marginLeft: "45%" }} />
+        </div>
+        <div style={{ width: "100%", backgroundImage: "linear-gradient(#ffffff, #eddfcb)" }}>
+
+            <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_6_desktop_89f81470-5a48-4b29-a73f-e52e4e94a215_2000x.png?v=1686650906" alt="banner" width="50%" style={{ marginRight: "45%" }} />
+        </div>
+        <div style={{ width: "100%", backgroundImage: "linear-gradient(#ffffff, #f3e5e5)" }}>
+
+            <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_7_desktop_acb84546-2e5d-4f69-bd03-3fd2ca9740db_2000x.png?v=1686650930" alt="banner" width="75%" style={{ marginLeft: "20%" }} />
         </div>
     </div>
 }
 
-export {Homepage}
+export { Homepage }
