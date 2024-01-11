13a59dd3b8f4598df0de2d187e8db4e184d1bdf4
JahirPendhari <jahirpp1999@gmail.com>
2024-01-11 20:16:18 +0530

    Collecting Raw Material for Products

---
+++ boat-ui/src/Common/rowmaterial.js
@@ -0,0 +1,470 @@
+[
+    {
+        "rating": 4.8,
+        "title": "Airdopes 131",
+        "name": "Best seller Airdopes 131",
+        "ID": 1,
+        "salePrice": "₹899",
+        "regularPrice": "₹2,990",
+        "discount": "70% off",
+        "features": ["+5", "13mm drivers", "IWP Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "20 Hours",
+        "images":
+            ["https://www.boat-lifestyle.com/cdn/shop/products/ivory-white_600x.png?v=1639466536"
+               ,"https://www.boat-lifestyle.com/cdn/shop/files/131-6_600x.jpg?v=1702008197","https://www.boat-lifestyle.com/cdn/shop/products/FeatureImagesAD1314XChargecopy4_900x.jpg?v=1702008197",
+               "https://www.boat-lifestyle.com/cdn/shop/products/ad131FIAD131TypeCcopy4_900x.jpg?v=1702008197","https://www.boat-lifestyle.com/cdn/shop/products/FeatureImagesAD131BV5.0copy4_900x.jpg?v=1702008197",
+               "https://www.boat-lifestyle.com/cdn/shop/products/FeatureImagesAD131VAcopy4_900x.jpg?v=1702008197"
+            ]
+    },
+    {
+        "rating": 4.9,
+        "title": "Airdopes 181",
+        "name": "Best seller Airdopes 181",
+        "ID": 2,
+        "salePrice": "₹1,099",
+        "regularPrice": "₹2,990",
+        "discount": "63% off",
+        "features": ["+2", "Clear Calling", "BEAST™ Mode"],
+        "addToCart": "Add To Cart",
+        "playback": "40 Hours",
+        "images":
+            ["https://www.boat-lifestyle.com/cdn/shop/products/main_black_357b6c8e-1332-4864-bf5c-b83ca9caf4bc_600x.png?v=1641980343","https://www.boat-lifestyle.com/cdn/shop/products/AD181-FI_Black01_900x.png?v=1659000222"
+              ,"https://www.boat-lifestyle.com/cdn/shop/products/AD181-FI_Black02_1500x.png?v=1659000346","https://www.boat-lifestyle.com/cdn/shop/products/AD181-FI_Black03_900x.png?v=1659000346"
+              ,"https://www.boat-lifestyle.com/cdn/shop/products/AD181-FI_Black05_900x.png?v=1659000346"
+            ]
+    },
+    {
+        "rating": 4.9,
+        "title": "Airdopes 161",
+        "name": "Best seller Airdopes 161",
+        "ID": 3,
+        "salePrice": "₹999",
+        "regularPrice": "₹2,490",
+        "discount": "60% off",
+        "features": ["+3", "ASAP™ Charge", "IWP Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "20 Hours",
+        "images":
+            ["https://www.boat-lifestyle.com/cdn/shop/products/main_black_fa1c6ec3-93b7-443e-ae82-d5eeb34258f8_600x.png?v=1641206192","https://www.boat-lifestyle.com/cdn/shop/files/A161PP.386_600x.png?v=1698400100"
+              ,"https://www.boat-lifestyle.com/cdn/shop/files/Black4_600x.jpg?v=1702007944","https://www.boat-lifestyle.com/cdn/shop/files/Black2_600x.jpg?v=1702007944"
+              ,"https://www.boat-lifestyle.com/cdn/shop/files/Black3_600x.jpg?v=1702007944","https://www.boat-lifestyle.com/cdn/shop/files/Black1_600x.jpg?v=1702007944"
+            ]
+    },
+    {
+        "rating": 4.9,
+        "title": "Airdopes 115",
+        "name": "Airdopes 115",
+        "ID": 4,
+        "salePrice": "₹1,299",
+        "regularPrice": "₹2,990",
+        "discount": "57% off",
+        "features": ["+2", "13mm Drivers", "BEAST™ Mode"],
+        "addToCart": "Add To Cart",
+        "playback": "42 Hours",
+        "images":
+            ["https://www.boat-lifestyle.com/cdn/shop/products/115-main2_600x.png?v=1655368638","https://www.boat-lifestyle.com/cdn/shop/files/original-imagd3xdwhnggyap_416x.webp?v=1685450497"
+              ,"https://www.boat-lifestyle.com/cdn/shop/files/original-imagd3xdyyzgynpt_416x.webp?v=1686565859","https://www.boat-lifestyle.com/cdn/shop/files/original-imagd3xdgejpjbpe_416x.webp?v=1686565859"
+              ,"https://www.boat-lifestyle.com/cdn/shop/files/original-imagd3xdmdbzmgce_416x.webp?v=1686565859"
+            ]
+    },
+    {
+        "rating": 4.9,
+        "title": "Airdopes 141",
+        "name": "Best seller Airdopes 141",
+        "ID": 5,
+        "salePrice": "₹1,299",
+        "regularPrice": "₹4,490",
+        "discount": "71% off",
+        "features": ["+3", "Clear Calling", "ASAP™ Charge"],
+        "addToCart": "Add To Cart",
+        "playback": "45 Hours"
+    },
+    {
+        "rating": 4.9,
+        "title": "Airdopes 131 PRO",
+        "name": "Airdopes 131 PRO",
+        "ID": 6,
+        "salePrice": "₹999",
+        "regularPrice": "₹2,990",
+        "discount": "67% off",
+        "features": ["+2", "Clear Calling", "BEAST™ Mode"],
+        "addToCart": "Add To Cart",
+        "playback": "35 Hours"
+    },
+    {
+        "rating": 4.9,
+        "title": "Airdopes Alpha",
+        "name": "Airdopes Alpha",
+        "ID": 7,
+        "salePrice": "₹899",
+        "regularPrice": "₹3,490",
+        "discount": "74% off",
+        "features": ["ENx™ Technology", "ASAP™ Charge", "IWP™ Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "Gaming Earbuds"
+    },
+    {
+        "rating": 4.9,
+        "title": "Immortal 131",
+        "name": "Immortal 131",
+        "ID": 8,
+        "salePrice": "₹1,099",
+        "regularPrice": "₹3,490",
+        "discount": "69% off",
+        "features": ["Up to 40 Hours Playback", "BEAST™ Mode", "ENx™ Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "50 Hours"
+    },
+    {
+        "rating": 4.6,
+        "title": "Airdopes Atom 83",
+        "name": "Best seller Airdopes Atom 83",
+        "ID": 9,
+        "salePrice": "₹1,499",
+        "regularPrice": "₹3,490",
+        "discount": "57% off",
+        "features": ["+2", "13mm Drivers", "ASAP™ Charge"],
+        "addToCart": "Add To Cart",
+        "playback": "120 Hours"
+    },
+    {
+        "rating": 5.0,
+        "title": "Nirvana Ion",
+        "name": "Sale price₹2,299 Regular price₹7,990",
+        "ID": 10,
+        "discount": "71% off",
+        "features": ["ENx™ Technology", "BEAST™ Mode", "Dual EQ Modes"],
+        "addToCart": "Add To Cart",
+        "playback": "100 Hours"
+    },
+    {
+        "rating": 4.5,
+        "title": "Airdopes Hype",
+        "name": "Sale price₹1,599 Regular price₹5,490",
+        "ID": 11,
+        "discount": "71% off",
+        "features": ["ENx™ Technology", "BEAST™ Mode", "13mm Drivers"],
+        "addToCart": "Add To Cart",
+        "noiseCancellation": "Noise Cancellation"
+    },
+    {
+        "rating": 4.8,
+        "title": "Nirvana Ion ANC",
+        "name": "Sale price₹2,799 Regular price₹9,990",
+        "ID": 12,
+        "discount": "72% off",
+        "features": ["120 Hours Playback", "BEAST™ Mode", "ENx™ Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "100 Hours",
+        "coupon": "₹500 OFF ON BIG500"
+    },
+    {
+        "rating": 5.0,
+        "title": "Airdopes Max",
+        "name": "Sale price₹1,499 Regular price₹5,490",
+        "ID": 13,
+        "discount": "73% off",
+        "features": ["ENx™ Technology", "ASAP™ Charge", "BEAST™ Mode"],
+        "addToCart": "Add To Cart",
+        "playback": "50 Hours"
+    },
+
+    {
+        "rating": 4.8,
+        "title": "Immortal Katana Blade",
+        "name": "Sale price₹2,299 Regular price₹3,999",
+        "ID": 15,
+        "discount": "43% off",
+        "features": ["RGB Lights", "BEAST™ Mode", "ASAP™ Charge"],
+        "addToCart": "Add To Cart",
+        "playback": "42 Hours"
+    },
+    {
+        "rating": 5.0,
+        "title": "TRebel Airdopes 141",
+        "name": "Sale price₹1,399 Regular price₹4,490",
+        "ID": 16,
+        "discount": "69% off",
+        "features": ["Clear Calling", "ASAP™ Charge"],
+        "addToCart": "Add To Cart",
+        "playback": "40 Hours",
+        "newLaunch": "🎉 New Launch"
+    },
+    {
+        "rating": 4.8,
+        "title": "Airdopes 71",
+        "name": "Sale price₹999 Regular price₹3,990",
+        "ID": 17,
+        "discount": "75% off",
+        "features": ["BEAST™ Mode", "ENx™ Technology", "IPX4 Resistance"],
+        "addToCart": "Add To Cart",
+        "limitedEdition": "🎉 New Launch"
+    },
+    {
+        "rating": 4.7,
+        "title": "Airdopes 161 Hulk Edition",
+        "name": "Sale price₹999 Regular price₹2,490",
+        "ID": 18,
+        "discount": "60% off",
+        "features": ["ASAP™ Charge", "Type-C Interface", "40 Hours Playback"],
+        "addToCart": "Add To Cart",
+        "playback": "50 Hours"
+    },
+    {
+        "rating": 4.7,
+        "title": "Airdopes 100",
+        "name": "Sale price₹1,499 Regular price₹3,490",
+        "ID": 19,
+        "discount": "57% off",
+        "features": ["Clear Calling", "ASAP™ Charge", "Noise Cancellation"],
+        "addToCart": "Add To Cart"
+    },
+    {
+        "rating": 4.8,
+        "title": "Airdopes 161 ANC",
+        "name": "Sale price₹1,699 Regular price₹5,999",
+        "ID": 20,
+        "discount": "72% off",
+        "features": ["50 Hours Playback", "ENx™ Technology", "ASAP Fast Charge"],
+        "addToCart": "Add To Cart",
+        "limitedEdition": "🎉 New Launch"
+    },
+    {
+        "rating": 4.8,
+        "title": "Airdopes 161 Thor Edition",
+        "name": "Sale price₹999 Regular price₹2,490",
+        "ID": 21,
+        "discount": "60% off",
+        "features": ["ASAP™ Charge", "Type-C Interface", "40 Hours Playback"],
+        "addToCart": "Add To Cart",
+        "playback": "60 Hours",
+        "engravingAvailable": "✍️ Engraving Available"
+    },
+    {
+        "rating": 5.0,
+        "title": "Airdopes 138",
+        "name": "Sale price₹1,399 Regular price₹2,990",
+        "ID": 22,
+        "discount": "53% off",
+        "features": ["+5", "13mm Drivers", "boAt Signature Sound"],
+        "addToCart": "Add To Cart",
+        "playback": "30 Hours",
+        "newLaunch": "🎉 New Launch"
+    },
+    {
+        "rating": 4.4,
+        "title": "Immortal 100",
+        "name": "Sale price₹999 Regular price₹3,299",
+        "ID": 23,
+        "discount": "70% off",
+        "features": ["ENx™ Technology", "BEAST™ Mode", "IWP Technology"],
+        "addToCart": "Add To Cart",
+        "gamingEarbuds": "Gaming Earbuds",
+        "coupon": "₹500 OFF ON BIG500"
+    },
+    {
+        "rating": 5.0,
+        "title": "Immortal 141",
+        "name": "Sale price₹1,599 Regular price₹3,499",
+        "ID": 24,
+        "discount": "54% off",
+        "features": ["Up to 40 Hours Playback", "BEAST™ Mode", "IWP Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "40 Hours"
+    },
+    {
+        "rating": 4.9,
+        "title": "Best seller Airdopes 121 PRO",
+        "name": "Sale price₹1,299 Regular price₹2,990",
+        "ID": 25,
+        "discount": "57% off",
+        "features": ["+2", "ASAP™ Charge", "IWP Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "40 Hours",
+        "coupon": "₹200 OFF ON BIG200"
+    },
+    {
+        "rating": 4.8,
+        "title": "Immortal 121",
+        "name": "Sale price₹1,499 Regular price₹3,490",
+        "ID": 26,
+        "discount": "57% off",
+        "features": ["BEAST™ Mode", "Real RGB LEDs"],
+        "addToCart": "Add To Cart",
+        "playback": "35 Hours",
+        "limitedEdition": "🎉 New Launch"
+    },
+    {
+        "rating": 4.5,
+        "title": "Airdopes Ace",
+        "name": "Sale price₹1,399 Regular price₹3,490",
+        "ID": 27,
+        "discount": "60% off",
+        "features": ["ASAP™ Charge", "ENx™ Technology", "BEAST™ Mode"],
+        "addToCart": "Add To Cart",
+        "playback": "17 Hours",
+        "engravingAvailable": "✍️ Engraving Available"
+    },
+    {
+        "rating": 4.8,
+        "title": "Airdopes 163",
+        "name": "Sale price₹1,499 Regular price₹2,490",
+        "ID": 28,
+        "discount": "40% off",
+        "features": ["+1", "boAt Immersive Sound", "IWP Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "40 Hours"
+    },
+    {
+        "rating": 5.0,
+        "title": "Immortal 161",
+        "name": "Sale price₹1,199 Regular price₹3,499",
+        "ID": 29,
+        "discount": "66% off",
+        "features": ["ENx™ Technology", "BEAST™ Mode", "Blazing RGB Lights"],
+        "addToCart": "Add To Cart",
+        "limitedEdition": "💝 Limited Edition"
+    },
+    {
+        "rating": 5.0,
+        "title": "Airdopes 131 DC Edition",
+        "name": "Sale price₹1,299 Regular price₹2,990",
+        "ID": 30,
+        "discount": "57% off",
+        "features": ["Up to 60HRS Playback", "13mm Drivers", "IWP Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "50 Hours"
+    },
+    {
+        "rating": 4.8,
+        "title": "Airdopes 125",
+        "name": "Sale price₹1,299 Regular price₹4,490",
+        "ID": 31,
+        "discount": "71% off",
+        "features": ["BEAST™ Mode", "Bluetooth v5.3", "ENx™ Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "40 Hours"
+    },
+    {
+        "rating": 4.9,
+        "title": "Airdopes 190",
+        "name": "Sale price₹1,499 Regular price₹3,499",
+        "ID": 32,
+        "discount": "57% off",
+        "features": ["Bluetooth v5.3", "ENx™ Technology"],
+        "addToCart": "Add To Cart",
+        "limitedEdition": "₹300 OFF ON BIG300"
+    },
+    {
+        "rating": 4.8,
+        "title": "Immortal 121 Deadpool Edition",
+        "name": "Sale price₹1,699 Regular price₹3,999",
+        "ID": 33,
+        "discount": "58% off",
+        "features": ["ENx™ Technology", "40 Hours Playback", "Blazing RGB Lights"],
+        "addToCart": "Add To Cart",
+        "gamingEarbuds": "Gaming Earbuds"
+    },
+    {
+        "rating": 4.9,
+        "title": "Airdopes 191G",
+        "name": "Sale price₹1,399 Regular price₹3,490",
+        "ID": 34,
+        "discount": "60% off",
+        "features": ["Up to 30 Hours Playback", "Clear Calling", "BEAST™ Mode"],
+        "addToCart": "Add To Cart",
+        "playback": "50 Hours",
+        "coupon": "₹300 OFF ON BIG300"
+    },
+    {
+        "rating": 4.8,
+        "title": "Airdopes Crude",
+        "name": "Sale price₹1,499 Regular price₹4,990",
+        "ID": 35,
+        "discount": "70% off",
+        "features": ["ENx™ Technology", "BEAST™ Mode", "ASAP™ Charge"],
+        "addToCart": "Add To Cart",
+        "gamingEarbuds": "Gaming Earbuds"
+    },
+    {
+        "rating": 4.7,
+        "title": "Immortal 170",
+        "name": "Sale price₹1,199 Regular price₹3,499",
+        "ID": 36,
+        "discount": "66% off",
+        "features": ["40 Hours Playback", "ENx™ Technology", "BEAST™ Mode"],
+        "addToCart": "Add To Cart",
+        "playback": "40 Hours"
+    },
+    {
+        "rating": 4.5,
+        "title": "Immortal 125",
+        "name": "Sale price₹1,199 Regular price₹3,499",
+        "ID": 37,
+        "discount": "66% off",
+        "features": ["ENx™ Technology", "BEAST™ Mode", "ASAP™ Charge"],
+        "addToCart": "Add To Cart",
+        "noiseCancellation": "Noise Cancellation"
+    },
+    {
+        "rating": 4.8,
+        "title": "Airdopes 411 ANC",
+        "name": "Sale price₹2,499 Regular price₹4,990",
+        "ID": 38,
+        "discount": "50% off",
+        "features": ["Clear Calling", "10mm Drivers"],
+        "addToCart": "Add To Cart",
+        "playback": "20 Hours"
+    },
+    {
+        "rating": 4.8,
+        "title": "Airdopes 381",
+        "name": "Sale price₹2,199 Regular price₹4,990",
+        "ID": 39,
+        "discount": "56% off",
+        "features": ["+7 Smart Touch Controls"],
+        "addToCart": "Add To Cart",
+        "playback": "40 Hours"
+    },
+    {
+        "rating": 4.9,
+        "title": "Immortal 150",
+        "name": "Sale price₹1,499 Regular price₹3,499",
+        "ID": 40,
+        "discount": "57% off",
+        "features": ["Fancy LED Lights", "BEAST™ Mode", "ASAP™ Charge"],
+        "addToCart": "Add To Cart",
+        "gamingEarbuds": "Gaming Earbuds"
+    },
+    {
+        "rating": 4.8,
+        "title": "Airdopes 192",
+        "name": "Sale price₹1,999 Regular price₹3,490",
+        "ID": 41,
+        "discount": "43% off",
+        "features": ["Up to 30HRS Playback", "ASAP™ Charge", "13mm Drivers"],
+        "addToCart": "Add To Cart",
+        "limitedEdition": "💝 Limited Edition",
+    },
+    {
+        "rating": 5.0,
+        "title": "Airdopes 131 Black Panther Marvel Edition",
+        "name": "Sale price₹1,299 Regular price₹2,990",
+        "ID": 42,
+        "discount": "57% off",
+        "features": ["Up to 60 Hours Playback", "13mm Drivers", "IWP Technology"],
+        "addToCart": "Add To Cart",
+        "playback": "60 Hours"
+    },
+    {
+        "rating": 4.8,
+        "title": "Airdopes Sonik",
+        "name": "Sale price₹1,999 Regular price₹5,990",
+        "ID": 43,
+        "discount": "67% off",
+        "features": ["ASAP™ Charge", "Bluetooth v5.3", "ENx™ Technology"],
+        "addToCart": "Add To Cart",
+        "noiseCancellation": "Noise Cancellation"
+    }
+]
