f398fb6b70b3a43db97a5e3c056c55fcd3ecb6a7
JahirPendhari <jahirpp1999@gmail.com>
2024-01-18 21:13:50 +0530

    Sample Product page and Card Created

--- boat-ui/src/Components/SingleProduct.jsx
+++ boat-ui/src/Components/SingleProduct.jsx
@@ -1,16 +1,23 @@
 const SingleProduct =({ID,name,image,salePrice,regularPrice,discount,playback})=>{
     
     return <div style={{width:"100%",border:"1px solid red",display:"flex",
-    flexDirection:"column",textAlign:"left",borderRadius:"10px",marginBottom:"30px"}}>
-       <img src={image} alt="image"  width={"100%"}/>
-       <button style={{width:"70%" ,padding:"5px", margin:"auto",borderRadius:"10px",backgroundColor:"#fcc50b"}}>{playback} Playback</button>
-       <p style={{marginLeft:"20px",fontWeight:"bolder"}}> {name}</p>
-       <p style={{marginLeft:"20px",marginTop:"-10px",fontWeight:"bolder"}}>{salePrice}
-          <span style={{marginLeft:"10px",color:"#b2b9bf",fontWeight:"lighter"}}>{regularPrice}</span>
-          <span style={{marginLeft:"10px",color:"green",fontWeight:"lighter"}}>{discount}</span>
-       </p> 
+          flexDirection:"column",textAlign:"left",borderRadius:"10px",marginBottom:"30px"}}
+        >
+            <img src={image} alt="image"  width={"100%"}/>
+            <button style={{width:"70%" ,padding:"5px", margin:"auto",borderRadius:"10px",backgroundColor:"#fcc50b"}}>{playback} Playback</button>
+           
+            <p style={{marginLeft:"20px",fontWeight:"bolder"}}> {name}</p>
+          
+            <p style={{marginLeft:"20px",marginTop:"-10px",fontWeight:"bolder"}}>{salePrice}
+                <span style={{marginLeft:"10px",color:"#b2b9bf",fontWeight:"lighter"}}>{regularPrice}</span>
+                <span style={{marginLeft:"10px",color:"green",fontWeight:"lighter"}}>{discount}</span>
+            </p> 
 
-        <button style={{width:"90%" , margin:"auto",padding:"10px",borderRadius:"10px",fontSize:"16px",backgroundColor:"black",color:"white",marginBottom:"10px"}}>Add To Cart</button>
+            <button style={{width:"90%" , margin:"auto",padding:"10px",borderRadius:"10px"
+               ,fontSize:"16px",backgroundColor:"black",color:"white",marginBottom:"10px"}}
+            >
+                 Add To Cart
+            </button>
     </div>
 }
 
