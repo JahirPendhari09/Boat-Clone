4957aa7e41b878ada4c45caeab2bda99466946a9
JahirPendhari <jahirpp1999@gmail.com>
2024-02-10 21:09:06 +0530

    Re-structuring CSS

--- boat-ui/src/Pages/home/Home.jsx
+++ boat-ui/src/Pages/home/Home.jsx
@@ -31,8 +31,8 @@ const Homepage = () => {
         {/* <input type="text" placeholder="Search Product" value={searchValue} onChange={(e)=> setSearchValue(e.target.value)}/> */}
         <div className={styles.mainBanner}>
             <div>
-                <p style={{ fontSize: "84px", fontWeight: "initial" }}>True Wireless</p>
-                <p style={{ fontSize: "84px", textAlign: "left", marginTop: "-100px", fontWeight: "bold" }}>Earbuds</p>
+                <p style={{ fontWeight: "initial" }}>True Wireless</p>
+                <p style={{ textAlign: "left", marginTop: "-100px", fontWeight: "bold" }}>Earbuds</p>
                 <BannerButton textAlign={false} price="999"/>
             </div>
             <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_1_desktop_4c81b094-8292-4d54-8b20-5eb3b823a4e6_2000x.png?v=1686650857" alt="banner" width="50%" style={{ marginLeft: "50%" }} />
@@ -45,7 +45,7 @@ const Homepage = () => {
 
         <div className={styles.mainBanner} >
             <div>
-                <p style={{ fontSize: "84px", fontWeight: "initial",textAlign:"right"}}>Smart<span style={{ fontSize: "84px", textAlign: "left", marginTop: "-110px", fontWeight: "bold" }}>Watches</span></p>
+                <p style={{ fontWeight: "initial",textAlign:"right"}}>Smart<span style={{ fontSize: "84px", textAlign: "left", marginTop: "-110px", fontWeight: "bold" }}>Watches</span></p>
                 <BannerButton textAlign={true} price="1699"/>
             </div>
             <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_2_mob_390x.png?v=1686117497" alt="banner" width="50%" style={{ marginRight: "45%" }} />
@@ -57,7 +57,7 @@ const Homepage = () => {
 
         <div className={styles.mainBanner}>
             <div>
-                <p style={{ fontSize: "84px",fontWeight: "bold" }}>Neckbands</p>
+                <p style={{fontWeight: "bold" }}>Neckbands</p>
                 <BannerButton textAlign={false} price={"999"}/>
             </div>
             <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_3_mob_1282x.png?v=1686131082" alt="banner" width="50%" style={{ marginLeft: "45%" }} />
@@ -69,7 +69,7 @@ const Homepage = () => {
 
         <div className={styles.mainBanner}>
             <div>
-                <p style={{ fontSize: "84px",textAlign:"right", fontWeight:"bolder"}}>Headphones</p>
+                <p style={{textAlign:"right", fontWeight:"bolder"}}>Headphones</p>
                 <BannerButton textAlign={true} price="1199"/>
             </div>
             <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_4_desktop_e51eb905-3de3-4323-8dc3-b2362bd879e2_2000x.png?v=1686650793" alt="banner" width="50%" style={{ marginRight: "45%" }} />
@@ -81,7 +81,7 @@ const Homepage = () => {
 
         <div  className={styles.mainBanner}>
             <div>
-                <p style={{ fontSize: "84px"}}>Wireless<span style={{ fontWeight: "bolder" }}>Speakers</span></p>
+                <p>Wireless<span style={{ fontWeight: "bolder" }}>Speakers</span></p>
                 <BannerButton textAlign={false} price={"749"}/>
             </div>
             <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_5_desktop_993080a5-771b-4fbd-961a-a4dae9e55838_2000x.png?v=1686650885" alt="banner" width="40%" style={{ marginLeft: "45%" }} />
@@ -92,8 +92,8 @@ const Homepage = () => {
         </Suspense>
 
         <div className={styles.mainBanner}>
-            <div style={{ }}>
-                <p style={{ fontSize: "84px"}}>Party<span style={{ fontWeight: "bolder" }}>Speakers</span></p>
+            <div>
+                <p>Party<span style={{ fontWeight: "bolder" }}>Speakers</span></p>
                 <BannerButton textAlign={true} price="1199"/>
             </div>
             <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_6_desktop_89f81470-5a48-4b29-a73f-e52e4e94a215_2000x.png?v=1686650906" alt="banner" width="50%" style={{ marginRight: "45%" }} />
@@ -105,7 +105,7 @@ const Homepage = () => {
 
         <div className={styles.mainBanner}>
             <div>
-                <p style={{ fontSize: "84px", fontWeight:"bolder"}}>Sound Bars</p>
+                <p style={{ fontWeight:"bolder"}}>Sound Bars</p>
                 <BannerButton textAlign={false} price={"1599"}/>
             </div>
             <img src="https://www.boat-lifestyle.com/cdn/shop/files/img_7_desktop_acb84546-2e5d-4f69-bd03-3fd2ca9740db_2000x.png?v=1686650930" alt="banner" width="75%" style={{ marginLeft: "20%", marginTop:"200px" }} />
